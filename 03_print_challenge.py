5f996c68a8f7f7675a33fcbffe72ee3c28be1612
Muhammad Jamshaid <bnglagame@gmail.com>
2026-06-29 09:15:56 +0500

    Learned and Practice Print Challenge

---
+++ 03_print_challenge.py
@@ -0,0 +1,9 @@
+# Show
+#1 My Learing Path:
+#2   - Python Basics
+#3   - Data Engineering
+#4   - AI     
+# All 1-4 with single print
+
+
+print("My Learning Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
