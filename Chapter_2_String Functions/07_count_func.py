3e28b69cf9fa8db8cd76c9aaba4a7948d242e894
Muhammad Jamshaid <bnglagame@gmail.com>
2026-06-29 09:20:27 +0500

    Learned and Practice  Count Function

---
+++ Chapter_2_String Functions/07_count_func.py
@@ -0,0 +1,9 @@
+# count() it returns how many time a word appeared in text..
+# Syntax: variable.count("value want to search")
+
+text = """
+    I want to master Python.
+    Python is very powerfull.
+    Python can be used almost everywhere.
+"""
+print(text.count("Python"))
