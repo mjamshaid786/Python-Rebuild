62d0040702a64cb797e1bf3169a1a40fcfd118bc
Muhammad Jamshaid <bnglagame@gmail.com>
2026-06-29 09:19:05 +0500

    Learned and Practice String Functions

---
+++ Chapter_2_String Functions/05_StrFunc.py
@@ -0,0 +1,14 @@
+# 1. Built-in Functions..
+
+# 1. type() Used to check the data type of value...
+first_name = "Muhammad "
+last_name = "Ibrahim"
+print (type(first_name))
+print (first_name + last_name)
+
+# str() convert any value into string data type.
+
+age = 25
+print ("Your age is: " + str(age))
+
+# type is changes only in above print line, originally it will remain int untill assigned to a variable.
