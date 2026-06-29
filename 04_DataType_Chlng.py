9f63401f3e6f65c68b3208c7fa28c87ffa3ac3e6
Muhammad Jamshaid <bnglagame@gmail.com>
2026-06-29 09:17:12 +0500

    Learned and Practiced TypeCasting

---
+++ 04_DataType_Chlng.py
@@ -0,0 +1,19 @@
+# Create 5 variables -Each with different data type..
+# 1. age
+# 2. height
+# 3. name
+# 4. student or not
+# 5. something with no value 
+# print values, data types & length of all variables
+
+age = 25
+height = 5.5
+name = "Muhammad"
+student = True
+nothing = None
+
+print (age, type(age))
+print (height, type(height))
+print (name, type(name), len(name))
+print (student, type(student))
+print (nothing, type(nothing))
