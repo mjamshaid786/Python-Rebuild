e5f92a47d28c14bfe464ae265d23a6210f635375
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-06-29 09:21:05 +0500

    Learned and Practice Replace Function

---
+++ Chapter_2_String Functions/08_replace.py
@@ -0,0 +1,8 @@
+# replace() it exchange new value with old value..
+# Syntax: variable.replace("old", "new")
+
+date = "28/12/2003"
+print("Old Date: ", date)
+new_date = date.replace("/", "-")
+print("New Date: ", new_date)
+
