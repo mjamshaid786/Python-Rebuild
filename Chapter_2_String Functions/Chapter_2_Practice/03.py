c518fb46d35f8fa4551ca0fc5909f9f6106b8238
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-06-30 12:36:25 +0500

    init: age into days

---
+++ Chapter_2_String Functions/Chapter_2_Practice/03.py
@@ -0,0 +1,5 @@
+# Age in Days: User se unki umar (years) input lein aur batayein ke wo takreeban kitne din (days) zinda rahe hain (Assume 1 year = 365 days).
+
+age_in_years = int(input("Enter your age in years: "))
+
+print(f"You lived: {age_in_years  * 365 } Days")
