a6e6d1f3e95c27c7a1714a10ebf2274189c525ea
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-06-30 12:35:34 +0500

    init: Basic Calculator

---
+++ Chapter_2_String Functions/Chapter_2_Practice/02.py
@@ -0,0 +1,9 @@
+# Basic Calculator: User se do float numbers input lein. Unka sum, difference, product, aur quotient (/) print karein.
+
+num1 = float(input("Enter fisrt number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Qoutient: {num1 / num2}")
