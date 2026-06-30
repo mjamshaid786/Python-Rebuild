f2a9cad97ecd6ed9353e53e565c60400a2222d6b
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-06-30 12:38:40 +0500

    init: currency converter

---
+++ Chapter_2_String Functions/Chapter_2_Practice/08.py
@@ -0,0 +1,5 @@
+# 8. Currency Converter: User se PKR mein amount input lein. Use USD mein convert karein (فرض کریں 1 USD = 278 PKR) aur result show karein.
+
+amount = int(input("Enter you amount in PKR: "))
+USD = 278
+print(amount / USD)
