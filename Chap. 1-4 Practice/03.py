503e32d7c24d204bcfda7864c2bce377fcf2e442
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-06-30 12:41:38 +0500

    init: reverse word

---
+++ Chap. 1-4 Practice/03.py
@@ -0,0 +1,4 @@
+# Task 3 — Word Reverser Ask the user to enter any word. Print the word normally on one line, then print it reversed on the next line using slicing [::-1].
+
+word = input("Enter any word: ")
+print(f"Normal word: {word}\nReversed word: {word[::-1]}")
