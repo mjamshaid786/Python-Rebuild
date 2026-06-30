c89f733d090f54745b5634b57dcf879a019bead4
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-06-30 12:40:14 +0500

    init: personal greeting

---
+++ Chap. 1-4 Practice/01.py
@@ -0,0 +1,4 @@
+# Task 1 — Personal Greeting Ask the user for their name. Print a greeting like: Hello, John! Welcome to Python. Make sure the name is displayed in title case.
+
+name = input("Enter your name: ").title()
+print(f"Hello, {name}! Welcome to Python.")
