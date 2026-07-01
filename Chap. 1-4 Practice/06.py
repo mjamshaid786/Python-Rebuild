b9f5efaaed0362aee8a1e717770845861178b505
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-01 06:00:11 +0500

    init: concatenation

---
+++ Chap. 1-4 Practice/06.py
@@ -0,0 +1,4 @@
+# Task 6 — Uppercase & Lowercase Printer Ask the user to enter any sentence. Print the sentence once in ALL CAPS and once in all lowercase on separate lines.
+
+sentence = input("Enter any sentence: ")
+print(f"All Capital: {sentence.upper()}\nAll Lower: {sentence.lower()}")
