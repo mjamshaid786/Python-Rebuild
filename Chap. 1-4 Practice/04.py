c0f516d36bc3c1f4f7dc4d7573fde5016bec9b91
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-07-01 05:56:50 +0500

    init: character counter

---
+++ Chap. 1-4 Practice/04.py
@@ -0,0 +1,4 @@
+# Task 4 — Character Counter Ask the user to enter any word. Print the total number of characters in that word using len().
+
+word = len(input("Enter any word: "))
+print(f"There are {word} Characters in your word.")
