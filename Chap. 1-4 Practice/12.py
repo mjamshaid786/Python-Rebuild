04a246180c6ae945c2bdf5596050b225cb41a1c8
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-01 06:03:37 +0500

    init: simple calculator

---
+++ Chap. 1-4 Practice/12.py
@@ -0,0 +1,9 @@
+# Task 12 — Simple Calculator Ask the user to enter two numbers. Print the result of addition, subtraction, multiplication, and division on separate labeled lines.
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+print(f"Addition: {first_number + second_number}")
+print(f"Subtraction: {first_number - second_number}")
+print(f"Multiplication: {first_number * second_number}")
+print(f"Division: {first_number / second_number}")
