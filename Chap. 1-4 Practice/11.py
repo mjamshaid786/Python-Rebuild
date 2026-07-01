f374d7d7e9a68812cca2b853049c3ef9326d2ac7
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-07-01 06:03:17 +0500

    init:  indexing

---
+++ Chap. 1-4 Practice/11.py
@@ -0,0 +1,4 @@
+# Task 11 — First & Last Character Ask the user to enter any word. Print the first character using index [0] and the last character using index [-1].
+
+word = input("Enter any word: ")
+print(f"First Character: {word[0]}\nLast Character: {word[-1]}")
