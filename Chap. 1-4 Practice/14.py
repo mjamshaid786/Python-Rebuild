b5d77245cb5f092dffe71318baeaff798c2265cd
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-07-01 06:04:30 +0500

    init: odd even

---
+++ Chap. 1-4 Practice/14.py
@@ -0,0 +1,4 @@
+# Task 14 — Even Number Check Ask the user to enter a whole number. Print the result of number % 2 == 0. This will print True if even and False if odd.
+
+number = int(input("Enter a number: "))
+print(number % 2 == 0)
