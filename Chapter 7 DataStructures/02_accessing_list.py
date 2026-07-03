51a56c14b4da3b1eeca11cd308e77bb18a99101e
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-07-03 11:35:29 +0500

    init: accessing list content

---
+++ Chapter 7 DataStructures/02_accessing_list.py
@@ -0,0 +1,13 @@
+list = [
+    ['a', 'b','c', 'd'],
+    ['f', 'g', 'h', 'i'],
+    ['j', 'k', 'l', 'm']
+]
+
+print(list)
+print("-" * 20)
+print(list[1])
+print("-" * 20)
+print(list[-1][-1])
+print(list[2][0])
+print("-" * 20)
