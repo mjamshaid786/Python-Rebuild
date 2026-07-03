7a39951b094e978699af8c4894d081e9f31b98cb
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-07-04 00:13:24 +0500

    init: list function

---
+++ Chapter 7 DataStructures/05_list_func_methods_op.py
@@ -0,0 +1,13 @@
+list = [44, 67, 98, 44, 22, 54]
+
+print("Max: " ,max(list))
+print("Min: " ,min(list))
+print("Sum: " ,sum(list))
+print("Len: " ,len(list))
+print("-" * 20)
+print("All: " ,all([23, 4, 6, 0, 5]))
+print("Any: " ,any([1,0,0,0]))
+print("-" * 20)
+print(f"Count: {list.count(44)}")
+print("-" * 20)
+print(f"Index: {list.index(22)}")
