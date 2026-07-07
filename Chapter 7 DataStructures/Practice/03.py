a682e93692b6d668bc6365d7ce099d0d3e8f04b2
Muhammad Jamshaid <128154619+mj6237@users.noreply.github.com>
2026-07-07 20:14:44 +0500

    list methods

---
+++ Chapter 7 DataStructures/Practice/03.py
@@ -0,0 +1,12 @@
+cart = []
+
+cart.append("milk")
+cart.append("bread")
+cart.append("eggs")
+cart.append("butter")
+print(cart)
+print("-" * 40)
+print(f"First Item: {cart[0]}")
+print(f"Last Item: {cart[3]}")
+print(f"Item at index 2: {cart[2]}")
+print(f"Last Item with -ve Index: {cart[-1]}")
