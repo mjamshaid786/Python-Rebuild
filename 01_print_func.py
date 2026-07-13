36034f8b97c558eb955091af83c1e5c2ff2ba676
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-13 11:07:47 +0500

    moved

--- 01_print_func.py
+++
@@ -1,5 +0,0 @@
-# print() is built-in function....
-print ("Hi New Me...!")
-print ("--------------------")
-print ("  IT'S TIME TO DO  ")
-print ("--------------------")
--- 02_escape_seq.py
+++
@@ -1,9 +0,0 @@
-# print "Salam Muhammad" in the output
-
-print ('"Salam Muhammad"') # Use Different Quotes to print the string with quotes
-
-print ("\"Salam Muhammad\"")# Use Escape Sequence to print the string with quotes
-
-# Writing Path ( C:\User\Muhammad)
-
-print ("C:\\User\\Muhammad") # Use escape seq. \\ to add \ in the output.
