4ab3bb9c98d77c856e0dfadd821ee391add3db1c
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-13 11:25:47 +0500

    init: 2d arrays

---
+++ Chapter_11_NumPy/Introduction/Creation/02_2D_array.py
@@ -0,0 +1,16 @@
+'''---2D array---'''
+import numpy as np
+
+arr_2d = np.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+print(arr_2d)
+
+# ZEROS ARRAY
+
+zeros = np.zeros(3) # np.zeros(shape) is used to create an array of zeros for future values, (n) for 1D, (n, n) for 2D
+print(zeros)
+
+zeros2d = np.zeros((3, 3))
+print(zeros2d)
+
