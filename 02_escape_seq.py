1e1f059f97a9bf792e4e836bba020a1dee27673b
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-22 22:08:36 +0500

    Create 02_escape_seq.py

---
+++ 02_escape_seq.py
@@ -0,0 +1,9 @@
+# print "Salam Muhammad" in the output
+
+print ('"Salam Muhammad"') # Use Different Quotes to print the string with quotes
+
+print ("\"Salam Muhammad\"")# Use Escape Sequence to print the string with quotes
+
+# Writing Path ( C:\User\Muhammad)
+
+print ("C:\\User\\Muhammad") # Use escape seq. \\ to add \ in the output.
