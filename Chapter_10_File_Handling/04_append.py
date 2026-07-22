c8bb791236139467737c7242b4af719c7c68397f
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-22 22:01:55 +0500

    Create 04_append.py

---
+++ Chapter_10_File_Handling/04_append.py
@@ -0,0 +1,5 @@
+# append method add new data next to previous data it do not delete the previous data..
+
+
+with open('file.txt', 'a') as file:
+    new_data = file.write("\nNew data is added.")                     
