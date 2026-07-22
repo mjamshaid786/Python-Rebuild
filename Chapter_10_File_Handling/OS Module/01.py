8765b6c9ccded5a4ac20e046dbb6c365e246c53a
Muhammad Jamshaid <bnglagame@gmail.com>
2026-07-22 22:03:52 +0500

    Create 01.py

---
+++ Chapter_10_File_Handling/OS Module/01.py
@@ -0,0 +1,9 @@
+
+
+
+import os
+# print(os.getcwd()) # it will print current working directory
+os.chdir(r'D:\Python Learning\Python-Rebuild') #for changing directory
+
+#to list directories inside a cwd
+print(os.listdir())
