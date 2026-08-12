23f8b96a10d53563b7e122b7b22d881ab6d918a6
Muhammad Jamshaid <bnglagame@gmail.com>
2026-08-12 22:37:05 +0500

    code updated

--- Projects/05_Student_Grade_Analyzer.py
+++ Projects/05_Student_Grade_Analyzer.py
@@ -8,3 +8,6 @@ id = input("Enter your ID: ")
 # =======================================
 #        GETTING USER MARKS
 # ========================================
+python_marks = int(input("Enter your Python Marks: "))
+if python_marks < 0 or python_marks > 100:
+    print("Please Enter Valid Marks")
