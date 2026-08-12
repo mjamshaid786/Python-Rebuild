5ff2de501be89840599e3d23c456eff844b41d5c
Muhammad Jamshaid <bnglagame@gmail.com>
2026-08-12 22:29:38 +0500

    taking user name and id as input

---
+++ Projects/05_Student_Grade_Analyzer.py
@@ -0,0 +1,6 @@
+# ==================================================
+#               STUDENT GRADE ANALYZER
+# ==================================================
+
+name = input("Enter your name: ").title()
+id = input("Enter your ID: ")
