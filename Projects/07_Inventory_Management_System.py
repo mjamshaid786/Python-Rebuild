b0ee60385b72e71b520246a34d24cc7de5a2f34d
Muhammad Jamshaid <bnglagame@gmail.com>
2026-08-15 09:36:11 +0500

    code updated

--- Projects/07_Inventory_Management_System.py
+++ Projects/07_Inventory_Management_System.py
@@ -2,6 +2,7 @@
 #       ADD PRODUCT FUNCTION
 # ===================================
 def add_product():
+    #______________ PRODUCT ID _______________
     while True:
         try:
             product_id = int(input("Enter Product ID: "))
@@ -10,7 +11,7 @@ def add_product():
             print("Add positive values only!")
         except ValueError:
             print("Product ID contains only integers (101, 112, etc)")
-    
+    #______________ PRODUCT NAME _______________
     while True:
         product_name = input("Enter Product Name: ").strip().title()
         if not product_name:
@@ -20,6 +21,51 @@ def add_product():
             print("Product name can not contain numbers or special character!")
             continue
         break
+    #______________ PRODUCT CATEGORY _______________
+    while True:
+        product_category = input("Enter Product Category: ").strip().title()
+        if not product_category:
+            print("Product category can not be empty!")
+            continue       
+        if not product_category.replace(" ", "").isalpha():
+            print("Product category can not contain numbers or special character!")
+            continue
+        break
+    #______________ PRODUCT PRICE _______________
+    while True:
+        try:
+            product_price = int(input("Enter Product Price: "))
+            if not product_price:
+                print("Product price can not be empty!")
+                continue       
+            if product_price > 0:
+                break
+            print("Product price can not contain negative numbers")
+            continue
+        except ValueError:
+            print("Product price can not contains alphabets or special character")
+    #______________ PRODUCT QUANTITY _______________
+    while True:
+        try:
+            product_quantity = int(input("Enter Product Quantity: "))
+            if not product_quantity:
+                print("Product quantity can not be empty!")
+                continue       
+            if product_quantity >= 0:
+                print("Product addedd successfully!")
+                break
+            print("Product quantity can not contain negative numbers!")
+            continue
+        except ValueError:
+            print("Product quantity can not contains alphabets or special character")
+
+
+
+# ===================================
+#      DISPLAY PRODUCTS FUNCTION
+# ===================================
+
+
 
 
 
@@ -42,6 +88,7 @@ user_input = input('''
 match user_input:
     case "1":
         add_product()
+    case "2":
 
 
 
