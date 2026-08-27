eee006c46fb88927dbfee3f013353e6ebebf7e40
Muhammad Jamshaid <bnglagame@gmail.com>
2026-08-27 20:00:00 +0500

    code updated

---
+++ Projects/15_CSV_PostgreSQL_Data_Loader/creating_table.py
@@ -0,0 +1,28 @@
+#==============================================
+#           IMPORTING LIBRARIES
+#==============================================
+#-------------- IMPORTING FUNCTIONS -----------
+from creating_database import creating_database
+
+#-------------- FOR LOGGING ---------------
+import logging
+logger = logging.getLogger("project_15_logger")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+file_handler = logging.FileHandler("project_15.log",mode='a')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+#-------------- FOR CSV ---------------
+import csv
+#-------------- FOR PostgreSQL ---------------
+import psycopg2 # --> pip install psycopg2
+
+#-------------- FOR .ENV ---------------
+import os
+# import dotenv
+from dotenv import find_dotenv, load_dotenv
+dotenv_path = find_dotenv() #Find the .env folder automatically
