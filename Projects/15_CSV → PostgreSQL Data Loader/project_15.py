1e43e95d7572fd6c144e31fd83864fa95c92791b
Muhammad Jamshaid <bnglagame@gmail.com>
2026-08-28 08:44:02 +0500

    importing libraries

---
+++ Projects/15_CSV → PostgreSQL Data Loader/project_15.py
@@ -0,0 +1,20 @@
+#==============================================
+#           IMPORTING LIBRARIES
+#==============================================
+
+#-------------- FOR LOGGING ---------------
+import logging
+logger = logging.getLogger("mytestlogger")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+file_handler = logging.FileHandler("my_test_data.log",mode='a')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+#-------------- FOR CSV ---------------
+import csv
+#-------------- FOR PostgreSQL ---------------
+import psycopg2
