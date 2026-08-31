0271de25874f275615c2c4b8e40465fb47ee261e
Muhammad Jamshaid <bnglagame@gmail.com>
2026-08-31 20:00:00 +0500

    setting new project logging

--- Projects/18 CAPSTONE DATA PIPELINE/logger_config.py
+++ Projects/18 CAPSTONE DATA PIPELINE/logger_config.py
@@ -12,5 +12,3 @@ logger.addHandler(handler)
 file_handler = logging.FileHandler("project_18.log",mode='w')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-
-logger.info("this is logger_config")
