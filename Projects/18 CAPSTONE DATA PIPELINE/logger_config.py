aee99cb32774396e4092f1a57a4f5a23b219841c
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-01 08:51:38 +0500

    setting logger config

--- Projects/18 CAPSTONE DATA PIPELINE/logger_config.py
+++ Projects/18 CAPSTONE DATA PIPELINE/logger_config.py
@@ -2,3 +2,4 @@ import logging
 logger = logging.getLogger("project_18_logger")
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
