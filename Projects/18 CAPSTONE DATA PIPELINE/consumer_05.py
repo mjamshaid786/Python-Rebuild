58607b6aebe4c1aa4eb1c2cbf01dec3dd921cc85
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 20:00:00 +0500

    consumer setup

--- Projects/18 CAPSTONE DATA PIPELINE/consumer_05.py
+++ Projects/18 CAPSTONE DATA PIPELINE/consumer_05.py
@@ -10,9 +10,30 @@ consumer = Consumer({
     'group.id': 'my-simple-group',
     'auto.offset.reset': 'earliest'
 })
-# 2. Topic Subscribe Karein
+# 2. Topic Subscribe 
 topic_name = 'sales'
 consumer.subscribe([topic_name])
 
 print("Consumer Listening... (Press Ctrl+C to stop)")
+
+# 3. Message Listen Loop
+try:
+    while True:
+        msg = consumer.poll(1.0)  # After every 1 second ask kafka for messsage.
+
+        if msg is None:
+            continue
+        if msg.error():
+            continue
+
+        # Bytes ko String aur fir JSON me decode karein
+        data = json.loads(msg.value().decode('utf-8'))
+        
+        print(f"\nReceived -> Sender: {data['sender']} | Message: {data['message']}")
+
+except KeyboardInterrupt:
+    print("\nConsumer Closed.")
+finally:
+    consumer.close()
+
 
