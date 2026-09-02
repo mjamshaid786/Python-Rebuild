8dd9094a2471490e940d9eb0b259145de774d5c6
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 20:00:00 +0500

    working on data collection from api

--- Projects/18 CAPSTONE DATA PIPELINE/api_client.py
+++ Projects/18 CAPSTONE DATA PIPELINE/api_client.py
@@ -4,9 +4,15 @@ logger = logging.getLogger(f"project_18_logger.{__name__}")
 
 def getting_api_data():
     API_URL = "https://dummyjson.com/users"
-    reponse = requests.get(API_URL, timeout=10)
-    data = reponse.json()
-    print(data.keys())
+    try:
+        response = requests.get(API_URL, timeout=10)
+        response.raise_for_status()
+        return response.json
+    except requests.exceptions.ConnectionError:
+        print("Connection ERROR !")
+    except requests.exceptions.Timeout:
+        print("Timeout ERROR !")
+
 
 getting_api_data()
 
