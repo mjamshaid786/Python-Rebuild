ccf71826e8d5cab17bca1dd2130ce28c77dbd209
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 20:00:00 +0500

    consumer setup

--- Projects/18 CAPSTONE DATA PIPELINE/consumer_05.py
+++ Projects/18 CAPSTONE DATA PIPELINE/consumer_05.py
@@ -1,15 +1,14 @@
-import logging
-import logger_config
-logger = logging.getLogger(f"project_18_logger.{__name__}")
 import json
 from confluent_kafka import Consumer
-
+from psycopg2.extras import execute_values
+from table import conn
 # 1. Kafka Connection Setup
 consumer = Consumer({
     'bootstrap.servers': 'localhost:9092',
     'group.id': 'my-simple-group',
     'auto.offset.reset': 'earliest'
 })
+
 # 2. Topic Subscribe 
 topic_name = 'sales'
 consumer.subscribe([topic_name])
@@ -19,21 +18,36 @@ print("Consumer Listening... (Press Ctrl+C to stop)")
 # 3. Message Listen Loop
 try:
     while True:
-        msg = consumer.poll(1.0)  # After every 1 second ask kafka for messsage.
-
+        msg = consumer.poll(1.0)  
         if msg is None:
             continue
         if msg.error():
+            print("There is an Error", msg.error())
             continue
 
-        # Converting bytes back to json 
+
         data = json.loads(msg.value().decode('utf-8'))
         
-        print(f"\nReceived -> Sender: {data['sender']} | Message: {data['message']}")
+        print(f"""\nReceived\n 
+                Order ID    : {data['order_id']}
+                Price       : {data['price']}
+                Product Name: {data['product']}
+                Quantity    : {data['quantity']}
+                Total       : {data['total']} """)
+        with conn.cursor() as cur:
+            values = [
+                (data['order_id'], data['product'], data['price'], data['quantity'], data['total'])
+            ]
+            query = """
+            --sql
+            INSERT INTO sales (order_id, product, price, quantity, total) VALUES %s ON CONFLICT (order_id) DO NOTHING
+            ;
+            """
+            execute_values(cur, query, values)
+            conn.commit()
+            print("Saved to PostgreSQL")
 
 except KeyboardInterrupt:
     print("\nConsumer Closed.")
 finally:
     consumer.close()
-
-
