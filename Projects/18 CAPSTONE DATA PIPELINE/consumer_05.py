efb0f6645590149d13db79b4dcd6987e0febdf35
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 20:00:00 +0500

    consumer setup

--- Projects/18 CAPSTONE DATA PIPELINE/consumer_05.py
+++ Projects/18 CAPSTONE DATA PIPELINE/consumer_05.py
@@ -3,3 +3,10 @@ import logger_config
 logger = logging.getLogger(f"project_18_logger.{__name__}")
 import json
 from confluent_kafka import Consumer
+
+# 1. Kafka Connection Setup
+consumer = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'my-simple-group',
+    'auto.offset.reset': 'earliest'
+})
