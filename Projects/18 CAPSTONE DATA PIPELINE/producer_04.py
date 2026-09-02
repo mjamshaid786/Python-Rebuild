1feb909b667148dc635b120f9564a8d522d14c6e
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 21:05:30 +0500

    producer ready

--- Projects/18 CAPSTONE DATA PIPELINE/producer_04.py
+++ Projects/18 CAPSTONE DATA PIPELINE/producer_04.py
@@ -1,7 +1,7 @@
 import logging
 import logger_config
 logger = logging.getLogger(f"project_18_logger.{__name__}")
-
+from flattening_users_data_03 import data_flattening, users
 
 import json
 from confluent_kafka import Producer # for installng Run --> pip install confluent-kafka (verify using [pip list | Select-String "confluent-kafka"]
