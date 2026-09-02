b02f3e3bcccc249301d8ae4397b9be712bb51b62
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 09:28:05 +0500

    organizing file names

--- Projects/18 CAPSTONE DATA PIPELINE/main.py
+++ Projects/18 CAPSTONE DATA PIPELINE/main.py
@@ -2,8 +2,8 @@ import logging
 import logger_config
 logger = logging.getLogger(f"project_18_logger.{__name__}")
 #----- Importing Function From Other Files -----
-from api_client import getting_api_data
-from data_transformation import transforming_data, data
+from api_client_01 import getting_api_data
+from data_transformation_02 import transforming_data, data
 
 #==============================================
 #                   MAIN
