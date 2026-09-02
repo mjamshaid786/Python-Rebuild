8b3f67b72435ddd7c48cc8c55dc58e419d63074e
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 20:00:00 +0500

    working on data collection from api

--- Projects/18 CAPSTONE DATA PIPELINE/api_client.py
+++ Projects/18 CAPSTONE DATA PIPELINE/api_client.py
@@ -1,5 +1,12 @@
-import logging, requests
+import logging, requests, json
 import logger_config
 logger = logging.getLogger(f"project_18_logger.{__name__}")
 
+def getting_api_data():
+    API_URL = "https://dummyjson.com/users"
+    reponse = requests.get(API_URL, timeout=10)
+    data = json.load(reponse)
+    print(data)
+
+getting_api_data()
 
