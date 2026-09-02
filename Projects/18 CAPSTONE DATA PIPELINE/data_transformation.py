f6e73a653ceb5cc9bde94a988f1af3258f69850d
Muhammad Jamshaid <bnglagame@gmail.com>
2026-09-02 08:02:36 +0500

    working on data collection from api

--- Projects/18 CAPSTONE DATA PIPELINE/data_transformation.py
+++ Projects/18 CAPSTONE DATA PIPELINE/data_transformation.py
@@ -1,3 +1,9 @@
 import logging
+from main import data
 import logger_config
 logger = logging.getLogger(f"project_18_logger.{__name__}")
+
+if data:
+    logger.info("Data Received Successfully.")
+    clean_user = []
+
